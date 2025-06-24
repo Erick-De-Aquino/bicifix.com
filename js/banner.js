ff907bb98dfd2a8ff13fcf924d4990017f6a41c0
Erick De Aquino <Erickdeaquinoes@gmail.com>
2025-06-24 12:56:47 +0200

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -1,7 +1,7 @@
 const hero = document.getElementById('hero-content')
 const h1Banner = document.getElementById('h1Banner')
 
-fetch("../data/datos.json")
+fetch("data/datos.json")
     .then(response => response.json())
     .then(data =>{
         data.banner.map((link) => {
