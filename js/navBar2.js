3898ddec1279144e65d4d2cf32dd63a999d7c96e
Erick De Aquino <Erickdeaquinoes@gmail.com>
2025-06-26 13:08:14 +0200

    Update navBar2.js

--- js/navBar2.js
+++ js/navBar2.js
@@ -2,7 +2,7 @@ const navbar = document.getElementById('navBar2')
 fetch("../data/datos.json")
     .then(response => response.json())
     .then(data =>{
-        data.navItems.map((link) => {
+        data.navItems2.map((link) => {
             let pre = ""
             if(link.title === "INICIO"){
                 pre = ""
