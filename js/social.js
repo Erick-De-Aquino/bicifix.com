ecdc8f9b5150f325d16375a9756d2bfbead0adb2
Erick De Aquino <Erickdeaquinoes@gmail.com>
2025-06-26 14:28:54 +0200

    Update social.js

--- js/social.js
+++ js/social.js
@@ -1,7 +1,7 @@
 const iconos = document.getElementById('iconos')
 const acceso = document.getElementById('navBar2')
 console.log(acceso)
-fetch("data/datos.json")
+fetch("../data/datos.json")
     .then(response => response.json())
     .then(data =>{
         data.social.map((link) => {
